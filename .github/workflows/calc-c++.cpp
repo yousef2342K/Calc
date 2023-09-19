689a5ec6c32af54e6ba22379edec296e1f1da6b8
yousef2342K <47691080+yousef2342K@users.noreply.github.com>
2023-09-19 17:03:21 +0300

    Create calc-c++.cpp

---
+++ .github/workflows/calc-c++.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+double divide(int a, int b) {
+    if (b != 0) {
+        return static_cast<double>(a) / b;
+    }
+    else {
+        std::cout << "Error: Cannot divide by zero" << std::endl;
+        return 0.0;
+    }
+}
+
+int main() {
+    int a = 10;
+    int b = 5;
+
+    std::cout << "Addition: " << add(a, b) << std::endl;
+    std::cout << "Subtraction: " << subtract(a, b) << std::endl;
+    std::cout << "Multiplication: " << multiply(a, b) << std::endl;
+    std::cout << "Division: " << divide(a, b) << std::endl;
+
+    return 0;
+}
