07e2539bbb5a053afbac862f3a84fc729f4bff25
yousef2342K <47691080+yousef2342K@users.noreply.github.com>
2023-09-19 17:16:19 +0300

    Create pandaspr.py

---
+++ .github/workflows/pandaspr.py
@@ -0,0 +1,32 @@
+# Google step intern
+# code to predict customer churn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+# Load the dataset
+data = pd.read_csv('customer_data.csv')
+
+# Split the data into features (X) and target variable (y)
+X = data.drop('churn', axis=1)
+y = data['churn']
+
+# Perform feature scaling if necessary
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_scaled, y, test_size=0.2, random_state=42)
+
+# Train the model
+model = LogisticRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+accuracy = accuracy_score(y_test, y_pred)
+print("Accuracy:", accuracy
