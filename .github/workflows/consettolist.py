2b0a32b5a76b5ab59591163d50d4415b31864cb3
yousef2342K <47691080+yousef2342K@users.noreply.github.com>
2023-09-19 16:58:06 +0300

    Create consettolist.py

---
+++ .github/workflows/consettolist.py
@@ -0,0 +1,18 @@
+my_set = {"apple", "banana", "orange"}
+
+print(type(my_set)) #set
+
+
+my_list = list(my_set)
+
+
+
+print(type(my_list)) #list
+
+
+
+
+reversed_list = my_list[::-1]
+
+
+print(reversed_list)
